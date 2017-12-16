b085b535c91ae068126a347f168d2d44c5371434
shpotainna <shpota.inna@yandex.ua>
2017-12-16 18:07:31 +0200

    Added javadoc for a Periodical

--- src/main/java/com/inna/shpota/periodicals/domain/Periodicals.java
+++ src/main/java/com/inna/shpota/periodicals/domain/Periodicals.java
@@ -2,35 +2,82 @@ package com.inna.shpota.periodicals.domain;
 
 import java.math.BigDecimal;
 
+/**
+ * Periodicals Model Object
+ *
+ * <P>Various attributes of periodicals, and related behaviour.
+ *
+ * <P>Note that {@link BigDecimal} is used to model the month price - not double or float.
+ * See {@link #Periodicals(String name, String publisher, BigDecimal monthPrice)} or
+ * {@link #Periodicals(long id, String name, String publisher, BigDecimal monthPrice)} for more information
+ *
+ * @author Inna Shpota
+ * @version 1.0
+ */
 public class Periodicals {
     private long id;
     private final String name;
     private final String publisher;
     private final BigDecimal monthPrice;
 
+    /**
+     * Constructor for all parameters.
+     *
+     * @param id         ID of the periodicals.
+     * @param name       (required) Name of the periodicals.
+     * @param publisher  (required) Publisher of the periodicals.
+     * @param monthPrice (required) Month price of the all months.
+     */
     public Periodicals(long id, String name, String publisher, BigDecimal monthPrice) {
         this(name, publisher, monthPrice);
         this.id = id;
     }
 
+    /**
+     * Constructor.
+     *
+     * @param name       (required) Name of the periodicals.
+     * @param publisher  (required) Publisher of the periodicals.
+     * @param monthPrice (required) Month price of the all months.
+     */
     public Periodicals(String name, String publisher, BigDecimal monthPrice) {
         this.name = name;
         this.publisher = publisher;
         this.monthPrice = monthPrice;
     }
 
+    /**
+     * Getter for an id.
+     *
+     * @return id
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * Getter for a name.
+     *
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Getter for a publisher.
+     *
+     * @return publisher
+     */
     public String getPublisher() {
         return publisher;
     }
 
+    /**
+     * Getter for a month price.
+     *
+     * @return monthPrice
+     */
     public BigDecimal getMonthPrice() {
         return monthPrice;
     }
