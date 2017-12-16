7480d7ef7cf1c20ba45a82da0c289903f8d3046e
shpotainna <shpota.inna@yandex.ua>
2017-12-16 18:25:46 +0200

    Added javadoc for a Subscription

--- src/main/java/com/inna/shpota/periodicals/domain/Subscription.java
+++ src/main/java/com/inna/shpota/periodicals/domain/Subscription.java
@@ -2,6 +2,16 @@ package com.inna.shpota.periodicals.domain;
 
 import java.time.LocalDateTime;
 
+/**
+ * Subscription Model Object
+ *
+ * <P>Various attributes of subscription, and related behaviour.
+ *
+ * See {@link #builder()}
+ *
+ * @author Inna Shpota
+ * @version 1.0
+ */
 public class Subscription {
     private long id;
     private final long readerId;
@@ -22,26 +32,56 @@ public class Subscription {
         this.date = date;
     }
 
+    /**
+     * Getter for an id.
+     *
+     * @return id
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * Getter for a reader id.
+     *
+     * @return readerId
+     */
     public long getReaderId() {
         return readerId;
     }
 
+    /**
+     * Getter for a periodicals id.
+     *
+     * @return periodicalsId
+     */
     public long getPeriodicalsId() {
         return periodicalsId;
     }
 
+    /**
+     * Getter for a month quantity.
+     *
+     * @return monthQuantity
+     */
     public int getMonthQuantity() {
         return monthQuantity;
     }
 
+    /**
+     * Getter for a date.
+     *
+     * @return date
+     */
     public LocalDateTime getDate() {
         return date;
     }
 
+    /**
+     * Builders new {@link Builder} for a {@link Subscription}.
+     *
+     * @return Builder object
+     */
     public static Builder builder() {
         return new Builder();
     }
@@ -90,31 +130,66 @@ public class Subscription {
 
         private Builder() { }
 
+        /**
+         * Setter for an id.
+         *
+         * @param id ID of the subscription.
+         * @return Builder object
+         */
         public Builder id(long id) {
             this.id = id;
             return this;
         }
 
+        /**
+         * Setter for an reader id.
+         *
+         * @param readerId (required) ID of the reader.
+         * @return Builder object
+         */
         public Builder readerId(long readerId) {
             this.readerId = readerId;
             return this;
         }
 
+        /**
+         * Setter for an periodicals id.
+         *
+         * @param periodicalsId (required) ID of the periodicals.
+         * @return Builder object
+         */
         public Builder periodicalsId(long periodicalsId) {
             this.periodicalsId = periodicalsId;
             return this;
         }
 
+        /**
+         * Setter for a month quantity.
+         *
+         * @param monthQuantity (required) Quantity of the months.
+         * @return Builder object
+         */
         public Builder monthQuantity(int monthQuantity) {
             this.monthQuantity = monthQuantity;
             return this;
         }
 
+        /**
+         * Setter for a date.
+         *
+         * @param date Date of the subscription.
+         * @return Builder object
+         */
         public Builder date(LocalDateTime date) {
             this.date = date;
             return this;
         }
 
+        /**
+         * Builds a new Subscription.
+         *
+         * @return Subscription object
+         */
         public Subscription build() {
             return new Subscription(id, readerId, periodicalsId, monthQuantity, date);
         }
