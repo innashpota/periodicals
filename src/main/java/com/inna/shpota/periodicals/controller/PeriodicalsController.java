bee76b5ed046b512f3be68e98aa06677ca29f657
shpotainna <shpota.inna@yandex.ua>
2017-12-28 16:00:34 +0200

    Added strategy logic

--- src/main/java/com/inna/shpota/periodicals/controller/PeriodicalsController.java
+++ src/main/java/com/inna/shpota/periodicals/controller/PeriodicalsController.java
@@ -1,8 +1,8 @@
 package com.inna.shpota.periodicals.controller;
 
-import com.inna.shpota.periodicals.dao.AdminDao;
-import com.inna.shpota.periodicals.dao.FactoryDao;
-import com.inna.shpota.periodicals.domain.Admin;
+import com.inna.shpota.periodicals.strategies.RequestHandler;
+import com.inna.shpota.periodicals.strategies.Strategy;
+import org.apache.log4j.Logger;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -15,8 +15,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 import java.io.IOException;
 
-@WebServlet(name = "PeriodicalsController", urlPatterns = {"", "/home"})
+@WebServlet(name = "PeriodicalsController",
+        urlPatterns = {"", "/periodicals", "/periodicals/*", "/admin", "/admin/*", "/edit-periodicals",
+                "/create", "/edit-periodicals/*", "/login", "/signup", "/reader/*", "/profile"}
+) //don't write here
 public class PeriodicalsController extends HttpServlet {
+    private final static Logger LOGGER = Logger.getLogger(PeriodicalsController.class);
+    private RequestHandler requestHandler;
+
     @Override
     public void init() throws ServletException {
         try {
@@ -24,26 +30,32 @@ public class PeriodicalsController extends HttpServlet {
             DataSource dataSource = (DataSource) initContext.lookup(
                     "java:/comp/env/jdbc/periodicals"
             );
-            AdminDao adminDao = FactoryDao.createAdminDao(dataSource);
-            System.out.println("-----------------ALL------------------------");
-            System.out.println(adminDao.getAll());
-            System.out.println("-----------------INSERT------------------------");
-            long id = adminDao.create(new Admin("admin7", "admin7"));
-            System.out.println("ID = " + id);
-            System.out.println("-----------------ALL------------------------");
-            System.out.println(adminDao.getAll());
+            requestHandler = RequestHandler.construct(dataSource);
+            LOGGER.info("Using JDNI lookup got the DataSource: " + dataSource);
         } catch (NamingException e) {
+            LOGGER.error("NamingException occurred in PeriodicalsController", e);
             throw new ServletException(e);
         }
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/home.jsp").forward(request, response);
+        processRequest(request, response, "doGet");
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        super.doPost(request, response);
+        processRequest(request, response, "doPost");
+    }
+
+
+    private void processRequest(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            String method
+    ) throws IOException, ServletException {
+        LOGGER.info(method + "method has been called");
+        Strategy strategy = requestHandler.getStrategy(request);
+        strategy.handle(request, response);
     }
 }
