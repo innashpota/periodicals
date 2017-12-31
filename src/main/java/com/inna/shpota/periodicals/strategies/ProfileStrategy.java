8f3dbd55f1e9aaedbf0dcb989b96c5534c39d65f
shpotainna <shpota.inna@yandex.ua>
2017-12-31 15:04:13 +0200

    Added information about reader

--- src/main/java/com/inna/shpota/periodicals/strategies/ProfileStrategy.java
+++ src/main/java/com/inna/shpota/periodicals/strategies/ProfileStrategy.java
@@ -1,12 +1,14 @@
 package com.inna.shpota.periodicals.strategies;
 
 import com.inna.shpota.periodicals.dao.ReaderDao;
+import com.inna.shpota.periodicals.domain.Information;
 import com.inna.shpota.periodicals.domain.Reader;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class ProfileStrategy extends Strategy {
     private final ReaderDao readerDao;
@@ -19,7 +21,9 @@ public class ProfileStrategy extends Strategy {
 
     public void handle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Reader reader = (Reader) request.getSession().getAttribute("reader");
-
+        long id = reader.getId();
+        List<Information> information = readerDao.getInformationByReader(id);
+        request.getSession().setAttribute("information", information);
         request.getRequestDispatcher("/jsp/profile.jsp").forward(request, response);
     }
 }
