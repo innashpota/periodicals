e92abeec16251ce93282460a6f6283e889251a7f
shpotainna <shpota.inna@yandex.ua>
2018-01-06 21:32:34 +0200

    Added assert test

---
+++ src/test/java/com/inna/shpota/periodicals/util/AssertTest.java
@@ -0,0 +1,60 @@
+package com.inna.shpota.periodicals.util;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.math.BigDecimal;
+
+public class AssertTest {
+    private String message = "Error message";
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void notEmpty() {
+        String str = null;
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage(message);
+
+        Assert.notEmpty(str, message);
+    }
+
+    @Test
+    public void isPositiveInt() {
+        int number = -4;
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage(message);
+
+        Assert.isPositive(number, message);
+    }
+
+    @Test
+    public void isPositiveLong() {
+        long number = -4;
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage(message);
+
+        Assert.isPositive(number, message);
+    }
+
+    @Test
+    public void isPositiveBigDecimal() {
+        BigDecimal number = new BigDecimal("-2.00");
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage(message);
+
+        Assert.isPositive(number, message);
+    }
+
+    @Test
+    public void notNull() {
+        Object object = null;
+        String message = "Error message";
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage(message);
+
+        Assert.notNull(object, message);
+    }
+}
