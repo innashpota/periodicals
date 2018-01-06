e9fc58584f8263961db10dc245ac4d2abf7edd5b
shpotainna <shpota.inna@yandex.ua>
2018-01-07 00:34:38 +0200

    Added test for error strategy

---
+++ src/test/java/com/inna/shpota/periodicals/strategies/ErrorStrategyTest.java
@@ -0,0 +1,26 @@
+package com.inna.shpota.periodicals.strategies;
+
+import org.junit.Test;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class ErrorStrategyTest {
+    @Test
+    public void shouldHandle() throws Exception {
+        Strategy strategy = new ErrorStrategy();
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+        given(request.getRequestDispatcher("/jsp/error.jsp")).willReturn(dispatcher);
+
+        strategy.handle(request, response);
+
+        verify(dispatcher).forward(request, response);
+    }
+}
