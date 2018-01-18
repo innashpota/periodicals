b4b6f2583f358ed86475639a18b6d3a05aeacc36
shpotainna <shpota.inna@yandex.ua>
2018-01-18 23:16:14 +0200

    Added test for reader sign up strategy

---
+++ src/test/java/com/inna/shpota/periodicals/strategies/ReaderSignUpStrategyTest.java
@@ -0,0 +1,28 @@
+package com.inna.shpota.periodicals.strategies;
+
+import org.junit.Test;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class ReaderSignUpStrategyTest {
+    @Test
+    public void shouldHandle() throws ServletException, IOException {
+        ReaderSignUpStrategy strategy = new ReaderSignUpStrategy();
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+        given(request.getRequestDispatcher("/WEB-INF/jsp/signup.jsp")).willReturn(dispatcher);
+
+        strategy.handle(request, response);
+
+        verify(dispatcher).forward(request, response);
+    }
+}
